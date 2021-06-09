f3e541aed883519d8110dc10fbbfa12103d38cf3
JEFuchs <73438215+JEFuchs@users.noreply.github.com>
2021-06-09 16:27:33 -0500

    Updated plotting code

--- JEF_tempstuff/vis_data_testcode.m
+++ JEF_tempstuff/vis_data_testcode.m
@@ -1,4 +1,4 @@
-spatialResolution = 0.5;
+spatialResolution = 0.1;
 WorldSize = 10;
 spatialExtent = [WorldSize,WorldSize,WorldSize];
 R_input = 2;
@@ -6,7 +6,7 @@ R = R_input/spatialResolution;
 gridSize = spatialExtent./spatialResolution;
 
 sig = 1;
-vs = 2;
+vs = 3;
 
 %% Run numerical code
 energies = [];
@@ -16,5 +16,5 @@ AM = metricGPUGet_Alcubierre(0,vs,R,sig,gridSize);
 Z = met2den(AM);
 
 %% Test visfunction
-fun_visdata(Z,spatialExtent,'Alcubierre Metric')
+fun_visdata(AM,Z,spatialExtent,20,'Alcubierre Metric')
 
